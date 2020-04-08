1e4f5304602ceaef40c0e0e0e9471ad008b4ef77
Eric-Strady <strady60@gmail.com>
2020-04-08 18:23:53 +0200

    test

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer id="footer" class="row">
+	<div id="leftLinks" class="col-lg-5">
+			Qui sommes-nous ?
+	</div>
+	<div id="copyright" class="col-lg-2">
+		Copyright
+	</div>
+	<div id="rightLinks" class="col-lg-5">
+		S'inscrire / Se connecter
+	</div>
+</footer>
