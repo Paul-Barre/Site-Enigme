df02305db21583fceac098d9adf9aaaa98a052ec
Paul Barre <pl.barre@outlook.com>
2020-04-08 12:48:12 +0200

    first commit / init project

---
+++ index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<!-- Required meta tags -->
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	    <!-- Bootstrap CSS -->
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	    <link rel="stylesheet" type="text/css" href="style.css">
+
+	    <title>ESCAPE THE WEB</title>	
+	</head>
+	<body>
+		<div class="container-fluid text-center"> <!-- fluid prend toute la largeur -->
+			<header id="header" class="row">
+				<div id="headline" class="col-lg-12">
+					<div class="row">
+						<div id="logo" class="col-lg-2">Logo</div>
+						<div id="title" class="col-lg-3">ESCAPE THE WEB</div>
+						<div class="col-lg-7"></div>
+					</div>	
+					<div id="menu" class="row">
+						<div class="col-lg-2">Accueil</div>
+						<div class="col-lg-2">Règles</div>
+						<div class="col-lg-4">Jouer</div>
+						<div class="col-lg-2">Scores</div>	
+						<div class="col-lg-2">Vos retours sur le jeu</div>
+					</div>	
+				</div>					
+			</header>
+			<div id="mainContent" class="row">
+				<div id="leftNews" class="col-lg-2">
+					News
+				</div>
+				<div id="welcomePage" class="col-lg-8">
+					<h2>Welcome</h2>
+					<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+				</div>
+				<div id="logButtonZone" class="col-lg-2">
+					<div id="buttonZone" class="row">
+						<div id="button" class="col-lg-12">Button</div>
+					</div>
+				</div>	
+			</div>
+			<footer id="footer" class="row">
+				<div id="leftLinks" class="col-lg-5">
+						Qui sommes-nous ?
+				</div>
+				<div id="copyright" class="col-lg-2">
+					Copyright
+				</div>
+				<div id="rightLinks" class="col-lg-5">
+					S'inscrire / Se connecter
+				</div>		
+			</footer>
+		</div>		
+	</body>
+</html>	
