d60e6cbd98f75ff87815ff350369f9d5f31c056d
Paul Barre <pl.barre@outlook.com>
2020-04-17 16:26:08 +0200

    fixed css + 404 error page

---
+++ view/frontend/RulesView.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Escape The Web</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="../../public/css/style.css">
+</head>
+	<body>
+		<header id="logoTitle_and_navbar" class="container-fluid text-center">
+			<div id="logo_and_title" class="row">
+				<div id="title" class="col-lg-4 offset-lg-3">
+					ESCAPE THE WEB
+				</div>
+				<div id="logo" class="col-lg-2">
+					<img src="../../public/images/lock_icon.png" alt="lock_icon">
+				</div>
+			</div>
+			<nav id="navbar" class="navbar navbar-expand-xl navbar-dark bg-dark">
+				<ul class="navbar-nav col-lg-12">
+					<li class="nav-item col-lg-2"><a class="nav-link" href="#">REGLES</a></li>
+					<li class="nav-item col-lg-3"><a class="nav-link" href="#">SCORES</a></li>
+					<li class="nav-item col-lg-2 active"><a class="nav-link" href="#"><button id="playButton" type="button">JOUER</button></a></li>
+					<li class="nav-item col-lg-3"><a class="nav-link" href="#">S'INSCRIRE / SE CONNECTER</a></li><li class="nav-item col-lg-2"><a class="nav-link" href="#">VOS RETOURS</a></li>
+				</ul>	
+			</nav>
+		</header>
+		<section>
+			<h1 id="rulesTitle">Règles du jeu</h1>
+			<blockquote>Lorem Ipsum Lorem Ipsum</blockquote>
+		</section>
+		<footer id="footer" class="row container-fluid text-center">
+			<div id="leftLinks" class="col-lg-5">
+				<div class="row">
+					<div class="col-lg-12">
+						<a href="http://www.google.com">Règles du jeu</a>
+					</div>	
+				</div>
+				<div class="row">
+					<div class="col-lg-12">
+						<a href="#">Vos retours sur le jeu</a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-12">
+						<a href="#">Qui sommes-nous ?</a>
+					</div>
+				</div>
+				<span class="borderLeft"></span>
+			</div>
+			<div id="copyright" class="col-lg-2">
+				<div class="row">
+					<div class="col-lg-12">
+						<a id="siteTitle" href="#">ESCAPE THE WEB</a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-12">
+						Copyright &copy 2020
+					</div>
+				</div>
+			</div>
+			<div id="rightLinks" class="col-lg-5">
+				<div class="row">
+					<div class="col-lg-12">
+						<a href="#">S'inscrire / Se connecter</a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-12">
+						<a href="#">Nous contacter</a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-12">
+						<a href="#">Mentions légales</a>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
---
+++ view/frontend/errorView.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Escape The Web</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="../../public/css/style.css">
+</head>
+	<body>
+		<h1 id="title_404">ERROR 404</h1>
+		<h3 id="text_404">La page que vous cherchez n'existe pas</h3>
+	</body>
+</html>
